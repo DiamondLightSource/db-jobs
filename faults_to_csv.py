e68923acbc074eb44d74270035a90284f2e453b8
Karl Levik <KarlLevik@users.noreply.github.com>
2020-01-27 18:25:13 +0000

    Include fault URLs in report

--- faults_to_csv.py
+++ faults_to_csv.py
@@ -6,7 +6,7 @@
 from mariadbreport import MariaDBReport
 import logging
 
-headers = ['beamline', 'start time', 'end time', 'visit', 'system', 'component', 'subcomponent', 'lost hrs', 'total samples', 'collected samples', 'title', 'local contact(s)' ]
+headers = ['beamline', 'start time', 'end time', 'visit', 'system', 'component', 'subcomponent', 'lost hrs', 'total samples', 'collected samples', 'title', 'local contact(s)', 'URL' ]
 
 sql_template = """SELECT bs.beamLineName "{0}",
 f.beamtimelost_starttime "{1}",
@@ -19,7 +19,8 @@ sc.name "{6}",
 (SELECT count(*) FROM Dewar d INNER JOIN Container con ON con.dewarId = d.dewarId INNER JOIN BLSample bls ON bls.containerId = con.containerId WHERE d.firstExperimentId = bs.sessionId) "{8}",
 (SELECT count(distinct(dc.blSampleId)) FROM DataCollection dc WHERE bs.sessionId = dc.sessionId) "{9}",
 f.title "{10}",
-group_concat(per.givenName, ' ', per.familyName SEPARATOR ', ') "{11}"
+group_concat(per.givenName, ' ', per.familyName SEPARATOR ', ') "{11}",
+concat('https://ispyb.diamond.ac.uk/faults/fid/', f.faultId) "{12}"
 FROM BF_fault f
   INNER JOIN BLSession bs USING(sessionId)
   INNER JOIN Proposal p USING(proposalId)
@@ -29,7 +30,7 @@ FROM BF_fault f
   LEFT OUTER JOIN Session_has_Person shp ON shp.sessionId = bs.sessionId AND shp.role LIKE 'Local Contact%'
   LEFT OUTER JOIN Person per ON per.personId = shp.personId
 WHERE f.beamtimelost_starttime > now() - INTERVAL 14 DAY
-GROUP BY bs.beamLineName, f.beamtimelost_starttime, f.beamtimelost_endtime, concat(p.proposalCode, p.proposalNumber, '-', bs.visit_number), s.name, c.name, sc.name, f.title
+GROUP BY bs.beamLineName, f.beamtimelost_starttime, f.beamtimelost_endtime, concat(p.proposalCode, p.proposalNumber, '-', bs.visit_number), s.name, c.name, sc.name, f.title, f.faultId
 ORDER BY bs.beamLineName, f.beamtimelost_starttime
 """
 
