fc3d2c24cbfbcc7667ecfa90cd6600c0d27546c6
Karl Levik <KarlLevik@users.noreply.github.com>
2021-01-18 22:24:00 +0000

    New report: processing stats for samples in LB proposals

---
+++ lb_sample_processing_stats_to_csv.py
@@ -0,0 +1,17 @@
+#
+# Copyright 2019 Karl Levik
+#
+
+from mariadbreport import MariaDBReport
+import logging
+
+r = MariaDBReport(
+    "config.cfg",
+    db_section="ISPyBDB",
+    report_section="LBSamplesReport",
+    email_section="LBSamplesEmails",
+    log_level=logging.DEBUG
+)
+r.make_sql()
+r.create_report()
+# r.send_email("LB samples report", attach_report=False)
